2b0cea453b1ff24dd2e641837fdf998b0cb73ae9
resteve <resteve@zikzakmedia.com>
2015-12-01 10:56:45 +0100

    Tests

--- tests/test_account_invoice_contact.py
+++ tests/test_account_invoice_contact.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_invoice_contact module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_invoice_contact module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceContactTestCase(unittest.TestCase):
+class AccountInvoiceContactTestCase(ModuleTestCase):
     'Test Account Invoice Contact module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_contact')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_contact'
 
 
 def suite():
